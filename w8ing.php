cd8a38b73c30fa44e2d7a489584dcd85f682238b
Guillaume Molter <gmolter@bgcom.ch>
2013-10-24 14:44:17 -0400

    WPML, Hook and redirection conditons
    - Added support for WMPL translated sites.
    - Changed to plugin init hook to have more data about the current page
    - Added some redirection conditions to handle more usecases

--- w8ing.php
+++ w8ing.php
@@ -12,13 +12,18 @@
  
 require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'w8ing_admin.php');
 
-function w8ing_redirect(){
+function w8ing_redirect($wp_query){
 	
 	/*** We retreive the plugin's options - Array ***/
 	$options = get_option('w8ing_options');
 	
 	$landingURl=false;
 	$vip=false;
+	
+
+	if(function_exists('icl_object_id')) {
+		$options["pageID"] = icl_object_id($options["pageID"],'page',true);
+	}
 	
 	/*** We check that the visitor is not a white listed IP address or trying to reach a white listed  ***/
 	
@@ -34,29 +39,58 @@ function w8ing_redirect(){
 	if(isset($options["page_list"]) && $options["page_list"]!=""){
 		$post_list=explode(",", $options["page_list"]);
 		foreach($post_list as $post){
-			if(strpos(get_permalink($post),$_SERVER['REDIRECT_URL'])){
+			if($post==$wp_query->query_vars["page_id"]){
 				$vip=true;
 			}
 		}
 	}
 	
+	
+	
 	/*** We check if a landing page has been defined and if  ***/
-	if(isset($options["pageID"]) && $landingURl=get_permalink($options["pageID"]) && !$vip){
+	if(isset($options["pageID"])){
 		
-		/*** We check if the landing page redirection is active or not ***/
-		if(isset($options["active"]) && $options["active"]=="on"){
-			wp_redirect($landingURl, 302 );
-		}
-		/*** If not we check if we should redirect to the homepage ***/
-		elseif(isset($options["homeRedirect"]) && $options["homeRedirect"]=="on" && $_SERVER['REDIRECT_URL']==$landingURl){
-			wp_redirect(get_bloginfo("wpurl"), 302 );
-		}
+		$landingURl=get_permalink($options["pageID"]);
 		
+		if($landingURl && !$vip && !is_admin()){
+			
+			/*** We check if the landing page redirection is active or not ***/
+			if(isset($options["active"]) && $options["active"]=="on"){
+				
+				$redirect = true;
+				
+				if( isset($wp_query->query_vars) && isset($wp_query->query_vars["page_id"]) && $options["pageID"]==$wp_query->query_vars["page_id"]){
+					$redirect=false;
+				}
+				
+				if( isset($wp_query->queried_object) && isset($wp_query->queried_object->ID) && $options["pageID"]==$wp_query->queried_object->ID){
+					$redirect=false;
+				}
+				
+				if($redirect) wp_redirect($landingURl, 302 );
+			}
+			/*** If not we check if we should redirect to the homepage ***/
+			elseif(isset($options["homeRedirect"]) && $options["homeRedirect"]=="on"){
+			
+				$redirect=false;
+				
+				
+				if(isset($wp_query->query_vars) && isset($wp_query->query_vars["page_id"]) && $options["pageID"]==$wp_query->query_vars["page_id"]){
+					$redirect=true;
+				}
+				if(isset($wp_query->queried_object) && isset($wp_query->queried_object->ID) && $options["pageID"]==$wp_query->queried_object->ID){
+					$redirect=true;
+				}
+				
+				if($redirect) wp_redirect(get_bloginfo("wpurl"), 302 );
+				
+			}
+		}
 	}
 		
 }
 
 
-add_action('init', 'w8ing_redirect', 1);
+add_action('pre_get_posts', 'w8ing_redirect', 1);
 
 ?>
